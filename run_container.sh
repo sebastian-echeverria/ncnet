57680fd30e24427194ebb9b91248dbc68c868e70
Sebastian Echeverria <secheverria@sei.cmu.edu>
2021-06-25 15:41:54 -0400

    Mounting typo

--- run_container.sh
+++ run_container.sh
@@ -1,4 +1,4 @@
  #!/usr/bin/env bash
  docker run --rm --gpus=all --mount type=bind,source="$(pwd)"/datasets,target=/app/datasets \
-                            --mount type=bind,source="$(HOME)"/.torch/models,target=/root/.torch/models \
+                            --mount type=bind,source=$HOME/.torch/models,target=/root/.torch/models \
                             ncnet/ncnet-uav
