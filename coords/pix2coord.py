44686ab2d4a08ae9883e267c4bb8660f9c45f6eb
Sebastian Echeverria <secheverria@sei.cmu.edu>
2021-08-30 16:36:10 -0400

    Added function to check for deformed projection.

--- coords/pix2coord.py
+++ coords/pix2coord.py
@@ -47,6 +47,32 @@ def project_first_in_second(img1, src_pts, dst_pts):
     return dst, matchesMask
 
 
+def check_if_rectangular_like(pts, centroid):
+    """Checks if the points form a rectangle-ish shape."""
+    # First calculate distances between every corner and centroid.
+    dists = np.zeros(4)
+    for i in range(0, 4):
+        dists[i] = np.linalg.norm(pts[i] - centroid)
+    print(f"Distances: {dists}")
+
+    # Now calculate percentual differences between the previous distances. If there is a percentual difference
+    # higher than the threshold, it means the projections has a non-right-angle-ish corner.
+    ANGLE_PERCENT_THRESHOLD = 20
+    bad_shape = False
+    diffsp = np.zeros(4)
+    for i in range(0, 4):
+        diffsp[i] = abs(dists[i] - dists[(i+1)%4])/dists[i]*100
+        if diffsp[i] > ANGLE_PERCENT_THRESHOLD:
+            bad_shape = True
+    print(f"% Diffs: {diffsp}")
+
+    if bad_shape:
+        print("BAD SHAPE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        return False
+    else:
+        return True
+
+
 def points_to_coordinates(template_path, mosaic_path, src_pts, dst_pts):
     """Gets two images and two sets of matching points, and returns the projected corners of the image, and the GPS
     coordinates of its centroid."""
@@ -56,6 +82,7 @@ def points_to_coordinates(template_path, mosaic_path, src_pts, dst_pts):
 
     centroid = CoordinateConversor.calculate_centroid(projected_corners)
     print(f"Center: {centroid}")
+    check_if_rectangular_like(projected_corners, centroid)
 
     conversor = CoordinateConversor(mosaic_path)
     gps_coords = conversor.pixel_to_coord(centroid[0], centroid[1])
